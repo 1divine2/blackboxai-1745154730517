e86729439ca6d631b42e5ad82e382b550d7d1271
Solomon Abdul Divine Kamara <144345554+1divine2@users.noreply.github.com>
2025-04-20 13:19:26 +0000

    Add frontend-react/src/App.js 1745155164859

---
+++ frontend-react/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import CryptoTrade from './pages/CryptoTrade';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/trade" element={<CryptoTrade />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
